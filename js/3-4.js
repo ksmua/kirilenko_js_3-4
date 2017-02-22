5047a5249b4a6da130657c7ef7bff1031ba8c8c3
Sergii <ksmua85@gmail.com>
2017-02-23 00:49:50 +0200

    without this

--- js/3-4.js
+++ js/3-4.js
@@ -1,5 +1,19 @@
+// var test {
+// 	testTitle: "Тест по програмированию",
+// 	buttonLabel: "Проверить мои результаты",
+// 	cssSrc: "css/main.css"
+// }
+
+
 var testTitle = "Тест по програмированию";
+var buttonLabel = "Проверить мои результаты";
 var cssSrc = 'css/main.css';
+
+/*
+	To bild test we mast take question from some sours.
+	We do not know how many qustions and ansvers camin.
+	After work with source we must take like this object:
+*/
 var testList = {
 	question1: "Вопрос №1",
 	ansvers1: {
@@ -20,13 +34,14 @@ var testList = {
 		var3: 'Вариант ответа №3'
 	}
 } 
-var doc = document.body;
-	doc.className = 'test-body';
-var insertFragment = document.createDocumentFragment();
-// document.createDocumentFragment();
 
+/*
+	This function take as argument object with test questions
+	and bild dom fragment that can be added to test page
+*/
 function createTestFragment(list){
 	var olElem = document.createElement('ol'); //create ordered list
+	
 	for (var key in list){
 		//if qurent is answers
 		if ( typeof(list[key]) === "object") {
@@ -35,56 +50,91 @@ function createTestFragment(list){
 			for (subKey in subList){
 				var liElem = document.createElement('li'); //create tag li
 					liElem.className = 'ul-li-elem';
+
 					var labelElem = document.createElement('label'); //create tag label
 						labelElem.innerHTML = subList[subKey]; //set ansver text in teg label
+
 						var inputElem = document.createElement('input'); //create input field
 							inputElem.type = 'checkbox'; //input = checkbox (or can be radio)
 							inputElem.id = 'checkbox1';
+
 					labelElem.insertBefore(inputElem, labelElem.firstChild); //add input before label content
+
 				liElem.appendChild(labelElem); //add label in li
 				ulElem.appendChild(liElem); //add li in ul
 			}
 		} else { 
 			//if qurent is question
 		var olLiElem = document.createElement('li'); //create ordered list element
+			olLiElem.className = 'ol-elem';
+
 			var ulElem = document.createElement('ul');
 				ulElem.className = 'ul-elem';
 				ulElem.innerHTML = list[key]; //add qestion text in ul title
 		}
+
 	olLiElem.appendChild(ulElem); //add question list to ordered list element
-	olLiElem.className = 'ol-elem';
 	olElem.appendChild(olLiElem); //add ordered list element to ordered list
 	}
+
 	insertFragment.appendChild(olElem); //add ol in fragment
-//after that the final fragment will be added to body
+	//after that the final fragment will be added to body
 }
 
-//add stylesheet link in html head
+
+/*
+	This function append link to css file in to heder section  
+*/
 function addStyleSheetLink(linkSrc){
-var head = document.head;
-	var styleSheetLink = document.createElement('link');
-	styleSheetLink.rel = 'stylesheet';
-	styleSheetLink.type = 'text/css';
-	styleSheetLink.href = linkSrc;
+	var head = document.head;
+
+		var styleSheetLink = document.createElement('link');
+			styleSheetLink.rel = 'stylesheet';
+			styleSheetLink.type = 'text/css';
+			styleSheetLink.href = linkSrc;
+
 	head.appendChild(styleSheetLink);
 }
 
+/*
+	START
+*/
+var doc = document.body;
+	doc.className = 'test-body';
+
+var insertFragment = document.createDocumentFragment();
+
 addStyleSheetLink(cssSrc); //add stylesheet link in html head
 
-
-var wrapElem = document.createElement('div');
+var wrapElem = document.createElement('div'); // class = 'wrapper';
 	wrapElem.className = 'wrapper';
-	//class = 'wrapper';
+
 	var headElem = document.createElement('p');
 		headElem.className = 'test-header';
 		headElem.innerHTML = testTitle; //set title 'Тест по програмированию'
+
 	wrapElem.appendChild(headElem); //add p in wraper
 
+
 	//add testBody
-	createTestFragment(testList);
-	wrapElem.appendChild(insertFragment);
-//  insertFragment.appendChild(wrapElem); //add wraper in body
-	doc.appendChild(wrapElem);
+	createTestFragment(testList);// create test content
+
+	var form = document.createElement('form');
+		form.appendChild(insertFragment);//append test content to form
+
+		var button = document.createElement('button');//create button
+			button.className = "check-button";
+			button.value = "checkbutton";
+			button.innerHTML = buttonLabel;
+
+		form.appendChild(button);//append button to form
+	
+	wrapElem.appendChild(form); //add form to wrapper
+
+doc.appendChild(wrapElem);
+
+
+
 	// doc.style = 'margin-left: 98px';
 // doc.appendChild(insertFragment);
 
